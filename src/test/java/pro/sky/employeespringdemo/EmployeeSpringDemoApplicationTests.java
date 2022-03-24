714063fc3657440896223b798a13b0b91e67c0ac
Natali-Kremneva <van2gog6@gmail.com>
2022-03-24 22:57:57 +0300

    Initial commit

---
+++ src/test/java/pro/sky/employeespringdemo/EmployeeSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.employeespringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
