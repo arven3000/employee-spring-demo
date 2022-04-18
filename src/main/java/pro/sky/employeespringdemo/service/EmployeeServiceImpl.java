957d45ff23db995a168aca04cbc40f53548b4ec6
Natali-Kremneva <van2gog6@gmail.com>
2022-04-18 21:49:07 +0300

    ДЗ Библиотеки

--- src/main/java/pro/sky/employeespringdemo/service/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/employeespringdemo/service/EmployeeServiceImpl.java
@@ -1,11 +1,14 @@
 package pro.sky.employeespringdemo.service;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import pro.sky.employeespringdemo.dto.Employee;
 import pro.sky.employeespringdemo.exception.EmployeeIsPresentException;
 import pro.sky.employeespringdemo.exception.EmployeeNotFoundException;
+import pro.sky.employeespringdemo.exception.EmployeeTextDataException;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 @Service
@@ -20,17 +23,26 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee addEmployee(String firstName, String lastName, int departmentId, double salary) {
-        String key = firstName + " " + lastName;
+        if (!StringUtils.isAlpha(firstName) || !StringUtils.isAlpha(lastName)) {
+            throw new EmployeeTextDataException("Некорректные данные.");
+        }
+        String key = StringUtils.capitalize(firstName.toLowerCase(Locale.ROOT))
+                + " " + StringUtils.capitalize(lastName.toLowerCase(Locale.ROOT));
         if (employees.containsKey(key)) {
             throw new EmployeeIsPresentException("Сотрудник уже присутсвует.");
         }
-        employees.put(key, new Employee(firstName, lastName, departmentId, salary));
+        employees.put(key, new Employee(StringUtils.capitalize(firstName.toLowerCase(Locale.ROOT)),
+                StringUtils.capitalize(lastName.toLowerCase(Locale.ROOT)), departmentId, salary));
         return employees.get(key);
     }
 
     @Override
     public Employee deleteEmployee(String firstName, String lastName) {
-        String key = firstName + " " + lastName;
+        if (!StringUtils.isAlpha(firstName) || !StringUtils.isAlpha(lastName)) {
+            throw new EmployeeTextDataException("Некорректные данные.");
+        }
+        String key = StringUtils.capitalize(firstName.toLowerCase(Locale.ROOT))
+                + " " + StringUtils.capitalize(lastName.toLowerCase(Locale.ROOT));
         if (employees.containsKey(key)) {
             Employee employee = employees.get(key);
             employees.remove(key);
@@ -41,7 +53,11 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee findEmployee(String firstName, String lastName) {
-        String key = firstName + " " + lastName;
+        if (!StringUtils.isAlpha(firstName) || !StringUtils.isAlpha(lastName)) {
+            throw new EmployeeTextDataException("Некорректные данные.");
+        }
+        String key = StringUtils.capitalize(firstName.toLowerCase(Locale.ROOT))
+                + " " + StringUtils.capitalize(lastName.toLowerCase(Locale.ROOT));
         if (employees.containsKey(key)) {
             return employees.get(key);
         }
